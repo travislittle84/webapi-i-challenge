361942000efa80bd39c3b4129c3489088f2e932d
Travis Little <kingtrav@gmail.com>
2019-08-07 18:02:17 -0600

    express installed. post, get, get by id done

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // implement your API here
+const express = require('express')
+
+// Database
+const db = require('./data/db')
+
+const server = express()
+
+// Middleware
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('Sup')
+})
+
+// POST
+server.post('/api/users', (req, res) => {
+    const newUser = req.body
+    if(!req.body.name || !req.body.bio){
+        res.status(400).json({
+            errorMessage: 'Please provide a name and bio for the user.'
+        })
+    }
+    db.insert(newUser)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'There was an error while saving the user to the database'
+            })
+        })
+})
+
+// GET all
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'The users information could not be retreived'
+            })
+        })
+})
+
+// GET by id
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params
+db.findById(id)
+    .then(foundUser => {
+        if(foundUser) {
+            res.json(foundUser)
+        } else {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exisit.'
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: 'The user information could not be retreived'
+        })
+    })
+})
+
+
+
+//END
+server.listen(4000, () => {
+    console.log("Server is online on port 4000...")
+})
